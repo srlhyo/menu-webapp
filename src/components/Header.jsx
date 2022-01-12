b2b589d4e7fd1b664e1e64fd5acb6a9c74d8654c
Olga <olga_ze@inbox.lv>
2022-01-13 00:08:54 +0200

    header collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import Logo from './Logo';
 
 function Header(props) {
+    const [y, setY] = React.useState(window.scrollY);
+    const header = React.useRef(null);
+
+const handleNavigation = React.useCallback(
+  e => {
+    const window = e.currentTarget;
+    if (y < window.scrollY) {
+      header.current.classList.remove("h-48");
+      header.current.classList.add("h-0");
+    } 
+    
+    setY(window.scrollY);
+  }, [y]
+);
+
+React.useEffect(() => {
+  setY(window.scrollY);
+  window.addEventListener("scroll", handleNavigation);
+
+  return () => {
+    window.removeEventListener("scroll", handleNavigation);
+  };
+}, [handleNavigation]);
+
     return (
         <header>
-            <div style={{backgroundImage: `url(${props.img})`}} className="h-48 bg-cover bg-center flex items-end justify-center">
-                <Logo />
-                {console.log(props.img)};
-                
+            <div ref={header} style={{backgroundImage: `url(${props.img})`}} className="h-48 bg-cover bg-center flex items-end justify-center transition-all duration-1000">
+                <Logo />  
             </div>
             <nav className="text-sm">
                 <ul className="list-none flex justify-evenly text-golden">
